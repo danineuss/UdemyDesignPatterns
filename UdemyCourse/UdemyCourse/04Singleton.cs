33b92e7b63299f6cbb77c096656e52904fc270c1
Dani Singer <daniel.singer.ch@gmail.com>
2019-05-27 13:47:38 +0300

    Finished with the Bridge Patterns

---
+++ UdemyCourse/UdemyCourse/04Singleton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Coding.Exercise
+{
+    public class SingletonTester
+    {
+        public static bool IsSingleton(Func<object> func)
+        {
+            var obj1 = func.Invoke();
+            var obj2 = func.Invoke();
+            bool test1 = obj1.Equals(obj2);
+            bool test2 = ReferenceEquals(obj1, obj2);
+            return test1 && test2;
+        }
+    }
+}
---
+++ UdemyCourse/UdemyCourse/06Bridge.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public interface IRenderer
+    {
+        string WhatToRenderAs { get; }
+    }
+
+    public class VectorRenderer : IRenderer
+    {
+        public string WhatToRenderAs => "lines";
+    }
+
+    public class RasterRenderer : IRenderer
+    {
+        public string WhatToRenderAs => "pixels";
+    }
+
+    public abstract class Shape
+    {
+        protected IRenderer renderer;
+        public string Name { get; set; }
+
+        protected Shape(IRenderer renderer)
+        {
+            this.renderer = renderer;
+        }
+
+        public override string ToString()
+        {
+            return $"Drawing {Name} as {renderer.WhatToRenderAs}";
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public Triangle(IRenderer renderer) : base(renderer)
+        {
+            Name = "Triangle";
+        }
+    }
+
+    public class Square : Shape
+    {
+        public Square(IRenderer renderer) : base(renderer)
+        {
+            Name = "Square";
+        }
+    }
+}
