797adfa8a6a167e4eb9d72f1d0e934d874af810a
Dani Singer <daniel.singer.ch@gmail.com>
2019-06-02 12:12:56 +0300

    Added recent exercises

---
+++ UdemyCourse/UdemyCourse/08Decorator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Bird
+    {
+        public int Age { get; set; }
+
+        public string Fly()
+        {
+            return (Age < 10) ? "flying" : "too old";
+        }
+    }
+
+    public class Lizard
+    {
+        public int Age { get; set; }
+
+        public string Crawl()
+        {
+            return (Age > 1) ? "crawling" : "too young";
+        }
+    }
+
+    public class Dragon // no need for interfaces
+    {
+        private Bird bird = new Bird();
+        private Lizard lizard = new Lizard();
+        private int age;
+        public int Age
+        {
+            get => age;
+            set
+            {
+                age = value;
+                bird.Age = value;
+                lizard.Age = value;
+            }
+        }
+
+        public Dragon(int age)
+        {
+            Age = age;
+        }
+
+        public string Fly()
+        {
+            return bird.Fly();
+        }
+
+        public string Crawl()
+        {
+            return lizard.Crawl();
+        }
+    }
+
+    public class Demo
+    {
+        public static void Main()
+        {
+            Dragon ancient = new Dragon(250);
+            Dragon infant = new Dragon(0);
+            Dragon warrior = new Dragon(7);
+
+            Console.WriteLine($"The {nameof(ancient)} is {ancient.Fly()} and {ancient.Crawl()}");
+            Console.WriteLine($"The {nameof(infant)} is {infant.Fly()} and {infant.Crawl()}");
+            Console.WriteLine($"The {nameof(warrior)} is {warrior.Fly()} and {warrior.Crawl()}");
+        }
+    }
+}
---
+++ UdemyCourse/UdemyCourse/10Flyweight.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Coding.Exercise
+{
+    public class Sentence
+    {
+        private string plainText;
+        private Dictionary<int, WordToken> tokenDict = new Dictionary<int, WordToken>();
+
+        public Sentence(string plainText)
+        {
+            this.plainText = plainText;
+        }
+
+        public WordToken this[int index]
+        {
+            get
+            {                
+                if (tokenDict.ContainsKey(index)) return tokenDict[index];
+                var token = new WordToken();
+                tokenDict.Add(index, token);
+                return token;
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            int index = 0;
+            foreach (var word in plainText.Split(' '))
+            {
+                if (tokenDict.ContainsKey(index))
+                {
+                    if (tokenDict[index].Capitalize)
+                    {
+                        sb.Append(word.ToUpper());
+                    }
+                    else
+                    {
+                        sb.Append(word);
+                    }
+                }
+                else
+                {
+                    sb.Append(word);
+                }
+                sb.Append(' ');
+                index++;
+            }
+            sb.Remove(sb.Length - 2, 1);
+            return sb.ToString();
+        }
+
+        public class WordToken
+        {
+            public bool Capitalize;
+        }
+    }
+
+
+    public class Demo
+    {
+        public static void Main()
+        {
+            Sentence sentence = new Sentence("hello world");
+            sentence[1].Capitalize = true;
+            Console.WriteLine(sentence);
+        }
+    }
+}
